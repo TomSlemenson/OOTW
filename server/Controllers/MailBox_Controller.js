97f8ded66fd6df84b3496ff50e71dae97dda198a
Tomas <tslemenson@gmail.com>
2020-02-05 22:52:18 -0800

    nodemailer works

--- server/Controllers/MailBox_Controller.js
+++ server/Controllers/MailBox_Controller.js
@@ -3,15 +3,25 @@ const nodemailer = require('nodemailer');
 const nodemailMailgun = require('nodemailer-mailgun-transport');
 
 const auth = {
-
+  service: 'gmail',
+  newline: 'unix',
   auth: {
-    api_key: '09b7f9e6cb4ba9aa8265d6dc1df547da-f8faf5ef-f9c04032',
-    domain: 'sandbox539142b9fd8341f39c2c2c83c373b3ca.mailgun.org'
+    user: 'ootwla@gmail.com',
+    pass: 'ootwla1234'
   }
 }
 
-const transporter = nodemailer.createTransport(nodemailMailgun(auth));
- 
+// const auth = {
+//   auth: {
+//     api_key: '09b7f9e6cb4ba9aa8265d6dc1df547da-f8faf5ef-f9c04032',
+//     domain: 'sandbox539142b9fd8341f39c2c2c83c373b3ca.mailgun.org'
+//   }
+// }
+
+const transporter = nodemailer.createTransport(auth);
+
+// const transporter = nodemailer.createTransport(nodemailMailgun(auth));
+
 module.exports = {
     sendEmailToBand: function (req, res) {
         const htmlText = 
